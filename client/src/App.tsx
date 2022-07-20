d0805dadee9b23711bd5c6bee5d9f09ae642969a
Artemka0-7 <pavlovskiy.07@inbox.ru>
2022-07-21 00:15:07 +0300

    full client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,28 @@
-import React, { FC, useContext, useEffect } from 'react';
+import React, { FC, useContext, useEffect, useState } from 'react';
 import { Context } from '.';
 import LoginForm from './components/loginForm'
 import {observer} from 'mobx-react-lite'
+import { IUser } from './models/IUser';
+import UserService from './service/UserService';
 
 const App: FC = () => {
   const {store} = useContext(Context)
+  const [users, setUsers] = useState<IUser[]>([])
   useEffect(() => {
     if (localStorage.getItem("token")) {
       store.checkAuth()
     }
   }, [])
+
+  const getUsers = async () => {
+    try{
+      const response = await UserService.fetchUsers();
+      setUsers(response.data)
+
+    } catch(e){
+      console.log(e)
+    }
+  }
 
   if (store.isLoading){
     return(
@@ -26,7 +39,14 @@ const App: FC = () => {
   return (
     <>
       <h1>{store.isAuth ? `Пользователь ${store.user.email} авторизован` : 'Авторизуйтесь!'}</h1>
+      <h1>{store.user.isActivated ? `Почта ${store.user.email} подтверждена` : 'Подтвердите почту!'}</h1>
       <button onClick={() => store.logout()}>Выйти</button>
+      <div>
+        <button onClick={() => getUsers()}>Получить пользователей</button>
+        {users.map(user => (
+          <div key={user.email}>{user.email}</div>
+        ))}
+      </div>
     </>
   );
 }
--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosRequestConfig } from 'axios'
+import { AuthResponse } from '../models/response/AuthResponse';
 
 export const API_URL = "http://localhost:8080/api"
 
@@ -7,9 +8,26 @@ const $api = axios.create({
   baseURL: API_URL
 }) 
 
-$api.interceptors.request.use((config: AxiosRequestConfig) => {
-  config.headers!.Authorization = `Bearer ${localStorage.getItem('token')}`;
+$api.interceptors.request.use((config) => {
+  config.headers!.Authorization = `Bearer ${localStorage.getItem('token')}`
   return config;
+})
+
+$api.interceptors.response.use((config) => {
+  return config;
+},async (error) => {
+  const originalRequest = error.config;
+  if (error.response.status == 401 && error.config && !error.config._isRetry) {
+      originalRequest._isRetry = true;
+      try {
+          const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {withCredentials: true})
+          localStorage.setItem('token', response.data.accessToken);
+          return $api.request(originalRequest);
+      } catch (e) {
+          console.log('НЕ АВТОРИЗОВАН')
+      }
+  }
+  throw error;
 })
 
 export default $api;
